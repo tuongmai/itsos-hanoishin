9a7704f442d0ac56f658b1b3e9ada5117f47c3fd
helscarthe <nguyenduytuan52@gmail.com>
2023-12-07 10:45:26 +0700

    add search tour guide by skill API

--- back-end/controllers/tourGuide.js
+++ back-end/controllers/tourGuide.js
@@ -1,5 +1,6 @@
 import { TourGuideSkill } from "../../models";
 import { Account } from "../../models";
+import { Op } from "sequelize";
 
 const TourGuideController = {
   tourGuideList: async (req, res) => {
@@ -30,6 +31,43 @@ const TourGuideController = {
           ],
         where: {
           user_id: userId,
+        },
+      });
+      res.status(201).json(tourGuide);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+  searchBySkillName: async (req, res) => {
+    try {
+      const skillName = req.param("name");
+      const tourGuideSkillQuery = await Account.findAll({
+        include: [
+            {
+              model: TourGuideSkill,
+              required: true,
+              where: {
+                skill: {
+                  [Op.like]: '%' + skillName + '%'
+                },
+              },
+            },
+          ],
+        attributes: ['user_id'],
+      });
+      const tourGuideIds = tourGuideSkillQuery.map(x => x.user_id)
+      const tourGuide = await Account.findAll({
+        include: [
+            {
+              model: TourGuideSkill,
+              required: true,
+            },
+          ],
+        where: {
+          user_id: {
+            [Op.in]: tourGuideIds
+          },
         },
       });
       res.status(201).json(tourGuide);
--- back-end/routes/tourGuide.js
+++ back-end/routes/tourGuide.js
@@ -10,4 +10,5 @@ export default function tourGuide(app) {
   router.get( "/", TourGuideController.tourGuideList);
   // GET {baseURL}/api/account/:id
   router.get( "/:id", TourGuideController.getById);
+  router.get( "/skill/:name", TourGuideController.searchBySkillName);
 }
